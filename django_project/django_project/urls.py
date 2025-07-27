124b9a1728c1854fa56d95df0184fd20c9bb24a5
FernandoSB <fernandosb6002@gmail.com>
2025-07-27 20:51:04 +0100

    4th commit

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users.views import register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('blog.urls')),
 ]
