d5d6ca9a61ffe56af27fcdf3912ccd498a65d9e3
FernandoSB <fernandosb6002@gmail.com>
2025-07-30 20:44:42 +0100

    commit

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users.views import register, profile, logout_view
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
